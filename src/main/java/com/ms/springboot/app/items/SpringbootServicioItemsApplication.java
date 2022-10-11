7b1078b7483f26ec6b9bbe5dc1a91e4b9e12fc43
Adriel Alejandro Aliaga Benavides <aaliagab85@gmail.com>
2022-10-11 17:14:17 -0400

    cambiando Hystrix por resilience4j

--- src/main/java/com/ms/springboot/app/items/SpringbootServicioItemsApplication.java
+++ src/main/java/com/ms/springboot/app/items/SpringbootServicioItemsApplication.java
@@ -2,11 +2,9 @@ package com.ms.springboot.app.items;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableCircuitBreaker
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
--- src/main/java/com/ms/springboot/app/items/controllers/ItemController.java
+++ src/main/java/com/ms/springboot/app/items/controllers/ItemController.java
@@ -4,19 +4,24 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 //import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ms.springboot.app.items.models.Item;
 import com.ms.springboot.app.items.models.Producto;
 import com.ms.springboot.app.items.services.IItemService;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 //@RequestMapping("/microservicio/items")
 public class ItemController {
+	
+	@Autowired
+	private CircuitBreakerFactory cbFactory;
 	
 	@Autowired
 	@Qualifier("serviceFeign")
@@ -24,14 +29,19 @@ public class ItemController {
 	private IItemService itemService;
 	
 	@GetMapping("/")
-	public List<Item> listadoItems(){
+	public List<Item> listadoItems(@RequestParam(name="nombre", required = false) String nombre, 
+			@RequestHeader(name="token-request", required = false) String token){
+		System.out.println("Nombre: "+nombre);
+		System.out.println("Token: "+token);
 		return itemService.findAll();
 	}
 	
-	@HystrixCommand(fallbackMethod = "metodoAlternativo")
+	//@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/id/{id}/cantidad/{cantidad}")
 	public Item obtenerItem(@PathVariable Long id, @PathVariable Integer cantidad){
-		return itemService.findById(id, cantidad);
+		return cbFactory.create("items")
+				.run(() -> itemService.findById(id, cantidad),
+						e -> metodoAlternativo(id, cantidad));
 	}
 	
 	public Item metodoAlternativo(Long id, Integer cantidad){
